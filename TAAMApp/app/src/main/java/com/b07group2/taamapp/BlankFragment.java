ed4460a7fba351c347cdacb8fc658e2958d722f5
chalkatom <adamchakour05@gmail.com>
2024-07-14 20:11:19 -0400

    feat: added functional home screen

--- TAAMApp/app/src/main/java/com/b07group2/taamapp/BlankFragment.java
+++ TAAMApp/app/src/main/java/com/b07group2/taamapp/BlankFragment.java
@@ -8,11 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
