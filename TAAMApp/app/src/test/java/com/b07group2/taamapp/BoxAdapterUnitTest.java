dc5179cac59a7bff8261d387c28392d197302ee0
chalkatom <adamchakour05@gmail.com>
2024-07-18 19:29:39 -0400

    feat (tests): added unit tests for compatible BoxAdapter methods fix: fixed bug of onBackPressed override where pressing back would crash the app

--- TAAMApp/app/src/test/java/com/b07group2/taamapp/BoxAdapterUnitTest.java
+++ TAAMApp/app/src/test/java/com/b07group2/taamapp/BoxAdapterUnitTest.java
@@ -1,18 +1,13 @@
 package com.b07group2.taamapp;
 
-import androidx.recyclerview.widget.RecyclerView;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
+
 
 public class BoxAdapterUnitTest {
 
