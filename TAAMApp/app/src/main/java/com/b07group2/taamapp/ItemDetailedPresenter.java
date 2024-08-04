b3bccec569ad6e211f31b3692efb128f8996b258
ChuJordan <jor.chu@mail.utoronto.ca>
2024-08-04 18:30:56 -0400

    feat (tests): added ability to remove item and successfully made button fit on screen

--- TAAMApp/app/src/main/java/com/b07group2/taamapp/ItemDetailedPresenter.java
+++ TAAMApp/app/src/main/java/com/b07group2/taamapp/ItemDetailedPresenter.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class ItemDetailedPresenter {
     ItemDetailedView view;
     ArrayList<ItemCollection> items;
+    private ItemCollection item;
 
     public ItemDetailedPresenter(ItemDetailedView view) {
         this.view = view;
@@ -38,12 +39,13 @@ public class ItemDetailedPresenter {
                 }
             }
 
+            this.item = item;
+
             if (item == null) {
                 view.showWarning("Item not found");
                 view.setInformation(0, "Item not found", "Item not found", "Item not found", "Item not found");
                 return;
             }
-
 
             // set the information in the view
             int lot = item.getLotNumber();
@@ -71,5 +73,9 @@ public class ItemDetailedPresenter {
             }
 
         });
+    }
+
+    public ItemCollection getItem() {
+        return item;
     }
 }
