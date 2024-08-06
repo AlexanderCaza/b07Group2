a47b9885ed1cdad868ef51bb662d0e1df2b68f7b
Sadra <sadra3tarehdan@gmail.com>
2024-08-05 21:54:24 -0400

    feat: FetchMimeTypeTask has been updated to server several media files at the same time

--- TAAMApp/app/src/main/java/com/b07group2/taamapp/FetchMimeTypeTask.java
+++ TAAMApp/app/src/main/java/com/b07group2/taamapp/FetchMimeTypeTask.java
@@ -7,37 +7,43 @@ import android.util.Log;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import com.b07group2.taamapp.ItemDetailedView;
 
-public class FetchMimeTypeTask extends AsyncTask<String, Void, String> {
-    private Uri media;
+public class FetchMimeTypeTask extends AsyncTask<Void, Void, ArrayList<String>> {
+    private ArrayList<Uri> mediaList;
     private FetchMimeTypeCallback callback;
 
-    public FetchMimeTypeTask(Uri media, FetchMimeTypeCallback callback) {
-        this.media = media;
+    public FetchMimeTypeTask(ArrayList<Uri> media, FetchMimeTypeCallback callback){
+        this.mediaList = media;
         this.callback = callback;
     }
 
     @Override
-    protected String doInBackground(String... urls) {
-        String mimeType = null;
-        try {
-            HttpURLConnection connection = (HttpURLConnection) new URL(urls[0]).openConnection();
-            connection.setRequestMethod("HEAD");
-            connection.connect();
-            mimeType = connection.getContentType();
-        } catch (IOException e) {
-            e.printStackTrace();
+    protected ArrayList<String> doInBackground(Void... voids) {
+        ArrayList<String> mimeTypes = new ArrayList<>();
+        for (Uri media : mediaList) {
+            String mimeType = null;
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(media.toString()).openConnection();
+                connection.setRequestMethod("HEAD");
+                connection.connect();
+                mimeType = connection.getContentType();
+            } catch (IOException e) {
+                e.printStackTrace();
+                mimeType = "unknown";
+            }
+            mimeTypes.add(mimeType);
         }
-        return mimeType;
+        return mimeTypes;
     }
 
     @Override
-    protected void onPostExecute(String mimeType) {
-        Log.w("FetchMimeTypeTask", "Media: " + media.toString() + " " + mimeType);
+    protected void onPostExecute(ArrayList<String> mimeTypes) {
+        Log.w("FetchMimeTypeTask", "MediaList: " + mediaList.toString() + " " + mimeTypes);
         if (callback != null) {
-            callback.onMimeTypeFetched(media, mimeType);
+            callback.onMimeTypeFetched(mediaList, mimeTypes);
         }
     }
 }
